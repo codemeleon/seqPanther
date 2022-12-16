43cea45ace5533bd1d52cb40bd17acaa404094cf
codemeleon <anmol.kiran@gmail.com>
2022-12-16 12:53:49 +0000

    matplotlib

--- setup.py
+++ setup.py
@@ -5,12 +5,10 @@ setup(
     name="SeqPanther",
     version="0.0.1",
     install_requires=[
-        'biopython==1.80', 'click==7.1.2', 'numpy==1.22.1', 'pandas==1.5.2',
-        'pyfaidx==0.6.3.1', 'pysam==0.18.0'
+        'biopython>=1.80', 'click>=7.1.2', 'numpy>=1.22.1', 'pandas>=1.5.2',
+        'pyfaidx>=0.6.3.1', 'pysam>=0.18.0', 'matplotlib>=3.6.2'
     ],
     packages=find_packages(include=["seqPanther", "seqPanther.*"]),
-    # package_dir={"seqPanther": "seqPanther"},
-    # scripts=["bin/seqPanther"],
     entry_points={
         'console_scripts': ['seqpanther=seqPanther.seqPanther:run'],
     },
